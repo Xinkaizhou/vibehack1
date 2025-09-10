870fbf44e5731fa554d25efddf5b3e82b6bc4fee
xinkai zhou <1067654317@qq.com>
2025-09-11 01:38:51 +0800

    Initial commit

---
+++ vibehack1/vibehack1App.swift
@@ -0,0 +1,17 @@
+//
+//  vibehack1App.swift
+//  vibehack1
+//
+//  Created by Daniel on 2025/9/11.
+//
+
+import SwiftUI
+
+@main
+struct vibehack1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ vibehack1Tests/vibehack1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  vibehack1Tests.swift
+//  vibehack1Tests
+//
+//  Created by Daniel on 2025/9/11.
+//
+
+import Testing
+@testable import vibehack1
+
+struct vibehack1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
